9de277ada8823764bb14f1523cb747f0fef0fc1a
ken1286 <kindle1286@gmail.com>
2019-10-21 20:53:02 -0700

    database fixing

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -92,12 +92,15 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        dj_database_url.parse(config('DATABASE_URL'))
-    }
+    # 'default': {
+    #     # 'ENGINE': 'django.db.backends.sqlite3',
+    #     # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #     dj_database_url.parse(config('DATABASE_URL'))
+    # }
 }
+
+DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
+
 
 
 # Password validation
